77ea3d53b4c5956ccb67eb7bddbe012965b23d17
phi-line <phi.liney@gmail.com>
2018-02-13 21:11:28 -0800

    added 'latest commit card'

--- js/index.js
+++ js/index.js
@@ -1,24 +1,41 @@
-var modal;
-document.addEventListener('DOMContentLoaded', function(){
-    var modal = document.querySelector('#resume-modal').classList;
-}, false);
-
-
-function toggleModal(state) {
-    state ? modal.add('is-active') : modal.remove('is-active');
-}
-
 const base = "https://api.github.com/users/phi-line/";
 const url = new URL("repos", base), params = {sort: 'pushed', direction: 'desc'}
 Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
 
-fetch(url, {
+document.addEventListener('DOMContentLoaded', function(){
+    fetch(url, {
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      return {commits: data[0].commits_url.split('{')[0], repo: data[0]} // fight me
+    })
+    .then(ret => {
+      fetch(ret.commits, {
+      })
+      .then(resp => resp.json())
+      .then(commits => {
+        updateCommitCard(commits[0], ret.repo)
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}, false);
 
-  })
-  .then(resp => resp.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+
+function toggleModal(state) {
+    let modal = getElement('resume-modal');
+    state ? modal.add('is-active') : modal.remove('is-active');
+}
+
+function updateCommitCard(commit, repo) {
+  getElement('commit-name').innerHTML = repo.name;
+  getElement('commit-name').setAttribute('href', repo.html_url);
+  // getElement('commit-img').setAttribute('src', commit.author.avatar_url);
+  getElement('commit-date').innerHTML = moment(commit.commit.author.date).fromNow();
+  getElement('commit-msg').innerHTML = commit.commit.message;
+}
+
+function getElement(id){
+  return document.getElementById(id);
+}
